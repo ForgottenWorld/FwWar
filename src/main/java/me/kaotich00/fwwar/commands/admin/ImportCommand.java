8d932122a2439dca099208288e972913302e0ba4
Kaotich00 <simone.righi.96@gmail.com>
2020-11-14 12:56:35 +0100

    Finished importer

--- src/main/java/me/kaotich00/fwwar/commands/admin/ImportCommand.java
+++ src/main/java/me/kaotich00/fwwar/commands/admin/ImportCommand.java
@@ -10,6 +10,7 @@ import it.forgottenworld.fwechelonapi.discourse.DiscoursePost;
 import it.forgottenworld.fwechelonapi.services.DiscourseService;
 import me.kaotich00.fwwar.api.war.War;
 import me.kaotich00.fwwar.commands.api.AdminCommand;
+import me.kaotich00.fwwar.integration.DiscourseImporter;
 import me.kaotich00.fwwar.integration.DiscourseParser;
 import me.kaotich00.fwwar.message.Message;
 import me.kaotich00.fwwar.services.SimpleWarService;
@@ -19,7 +20,6 @@ import me.kaotich00.fwwar.war.WarFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.Map;
 
@@ -47,76 +47,8 @@ public class ImportCommand extends AdminCommand {
         }
 
         if(Bukkit.getPluginManager().getPlugin("FWEchelon") != null) {
-            FWEchelonApi api = (FWEchelonApi) Bukkit.getPluginManager().getPlugin("FWEchelon");
-            DiscourseService discourseIntegrationService = api.getDiscourseService();
-
-            Message.IMPORTER_INFO.send(sender, "Collecting data from Topic ID: " + topicId);
-            discourseIntegrationService.getPostsWithCustomNoticeTypeInTopic(Integer.parseInt(topicId), (postList) -> {
-                SimpleWarService warService = SimpleWarService.getInstance();
-                boolean firstNation = true;
-                War importedWar = null;
-
-                for(DiscoursePost post: postList) {
-                    DiscourseParser discourseParser = new DiscourseParser(post);
-                    Map<String,String> parsedValues = discourseParser.getParsedData();
-
-                    Message.IMPORTER_INFO.send(sender, "Found the following data: ");
-                    for(Map.Entry<String,String> entry: parsedValues.entrySet()) {
-                        Message.IMPORTER_INFO.send(sender, ChatColor.YELLOW + entry.getKey() + ": " + ChatColor.WHITE + entry.getValue());
-                    }
-
-                    // Create a new war
-                    if(firstNation) {
-                        Message.IMPORTER_INFO.send(sender, ChatColor.GREEN + "Creating a new war of type " + ChatColor.BOLD + parsedValues.get("tipologia"));
-
-                        Message.IMPORTER_INFO.send(sender, ChatColor.GREEN + "Successfully created war of type " + ChatColor.BOLD + parsedValues.get("tipologia"));
-
-                        // Add the first participant nation
-                        importedWar = WarFactory.getWarForType(WarTypes.valueOf(parsedValues.get("tipologia")));
-                    }
-
-                    Message.IMPORTER_INFO.send(sender, "Adding nations");
-                    String nationName = parsedValues.get("nazione");
-
-                    try{
-                        TownyAPI townyAPI = TownyAPI.getInstance();
-                        Nation nation = townyAPI.getDataSource().getNation(nationName);
-
-                        Message.IMPORTER_INFO.send(sender, "Adding nation " + ChatColor.GREEN + nation.getName() + ChatColor.GRAY + " to participants");
-
-                        Double price = Double.parseDouble(parsedValues.get("prezzo").replace("z", ""));
-                        if(!nation.getAccount().canPayFromHoldings(price)) {
-                            Message.NATION_NOT_ENOUGH_MONEY.send(sender, nation.getName());
-                            Message.IMPORTER_INFO.send(sender, ChatColor.RED + "Exiting, please fix the above errors");
-                            return;
-                        }
-                        nation.getAccount().withdraw(price, "Joining the war");
-
-                        Message.IMPORTER_INFO.send(sender, ChatColor.GREEN + "Successfully added nation " + ChatColor.GOLD + nationName);
-                        importedWar.addNation(nation);
-
-                        String[] participants = parsedValues.get("partecipanti").replace("[", "").replace("]", "").split(",");
-                        for(String playerName: participants) {
-                            Message.IMPORTER_INFO.send(sender, "Adding resident " + ChatColor.GREEN + playerName + ChatColor.GRAY + " to participants");
-                            Resident resident = townyAPI.getDataSource().getResident(playerName);
-
-                            importedWar.addPlayerToWar(resident.getTown(), resident.getUUID());
-                            Message.IMPORTER_INFO.send(sender, ChatColor.GREEN + "Successfully added " + ChatColor.GREEN + playerName + ChatColor.GREEN + " to participants");
-                        }
-                    } catch (NotRegisteredException e) {
-                        Message.IMPORTER_INFO.send(sender, ChatColor.RED + "Nation or player not registered on Towny, please fix the name and run the importer again");
-                        return;
-                    } catch (EconomyException e) {
-                        warService.deleteWar();
-                    }
-
-                    Message.IMPORTER_INFO.send(sender, ChatColor.GREEN + "War successfully imported, you may now proceed with default commands for the war type");
-
-                    warService.setCurrentWar(importedWar);
-                }
-            }, (error) -> {
-                sender.sendMessage(error);
-            });
+            DiscourseImporter importer = new DiscourseImporter(topicId, sender);
+            importer.importData();
         }
 
     }
---
+++ src/main/java/me/kaotich00/fwwar/integration/DiscourseImporter.java
@@ -0,0 +1,108 @@
+package me.kaotich00.fwwar.integration;
+
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.exceptions.EconomyException;
+import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
+import com.palmergames.bukkit.towny.object.Nation;
+import com.palmergames.bukkit.towny.object.Resident;
+import it.forgottenworld.fwechelonapi.FWEchelonApi;
+import it.forgottenworld.fwechelonapi.discourse.DiscoursePost;
+import it.forgottenworld.fwechelonapi.services.DiscourseService;
+import me.kaotich00.fwwar.api.war.War;
+import me.kaotich00.fwwar.message.Message;
+import me.kaotich00.fwwar.services.SimpleWarService;
+import me.kaotich00.fwwar.utils.WarTypes;
+import me.kaotich00.fwwar.war.WarFactory;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+import java.util.Map;
+
+public class DiscourseImporter {
+
+    String topicId;
+    CommandSender sender;
+
+    public DiscourseImporter(String topicId, CommandSender sender) {
+        this.sender = sender;
+        this.topicId = topicId;
+    }
+
+    public void importData() {
+        FWEchelonApi api = (FWEchelonApi) Bukkit.getPluginManager().getPlugin("FWEchelon");
+        DiscourseService discourseIntegrationService = api.getDiscourseService();
+
+        Message.IMPORTER_INFO.send(sender, "Collecting data from Topic ID: " + topicId);
+        discourseIntegrationService.getPostsWithCustomNoticeTypeInTopic(Integer.parseInt(topicId), (postList) -> {
+            SimpleWarService warService = SimpleWarService.getInstance();
+            boolean firstNation = true;
+            War importedWar = null;
+
+            for(DiscoursePost post: postList) {
+                DiscourseParser discourseParser = new DiscourseParser(post);
+                Map<String,String> parsedValues = discourseParser.getParsedData();
+
+                Message.IMPORTER_INFO.send(sender, "Found the following data: ");
+                for(Map.Entry<String,String> entry: parsedValues.entrySet()) {
+                    Message.IMPORTER_INFO.send(sender, ChatColor.YELLOW + entry.getKey() + ": " + ChatColor.WHITE + entry.getValue());
+                }
+
+                // Create a new war
+                if(firstNation) {
+                    Message.IMPORTER_INFO.send(sender, ChatColor.GREEN + "Creating a new war of type " + ChatColor.BOLD + parsedValues.get("tipologia"));
+
+                    Message.IMPORTER_INFO.send(sender, ChatColor.GREEN + "Successfully created war of type " + ChatColor.BOLD + parsedValues.get("tipologia"));
+
+                    // Add the first participant nation
+                    importedWar = WarFactory.getWarForType(WarTypes.valueOf(parsedValues.get("tipologia")));
+
+                    firstNation = false;
+                }
+
+                Message.IMPORTER_INFO.send(sender, "Adding nations");
+                String nationName = parsedValues.get("nazione");
+
+                try{
+                    TownyAPI townyAPI = TownyAPI.getInstance();
+                    Nation nation = townyAPI.getDataSource().getNation(nationName);
+
+                    Message.IMPORTER_INFO.send(sender, "Adding nation " + ChatColor.GREEN + nation.getName() + ChatColor.GRAY + " to participants");
+
+                    Double price = Double.parseDouble(parsedValues.get("prezzo").replace("z", ""));
+                    if(!nation.getAccount().canPayFromHoldings(price)) {
+                        Message.NATION_NOT_ENOUGH_MONEY.send(sender, nation.getName());
+                        Message.IMPORTER_INFO.send(sender, ChatColor.RED + "Exiting, please fix the above errors");
+                        return;
+                    }
+                    nation.getAccount().withdraw(price, "Joining the war");
+
+                    Message.IMPORTER_INFO.send(sender, ChatColor.GREEN + "Successfully added nation " + ChatColor.GOLD + nationName);
+                    importedWar.addNation(nation);
+
+                    String[] participants = parsedValues.get("partecipanti").replace("[", "").replace("]", "").split(",");
+                    for(String playerName: participants) {
+                        Message.IMPORTER_INFO.send(sender, "Adding resident " + ChatColor.GREEN + playerName + ChatColor.GRAY + " to participants");
+                        Resident resident = townyAPI.getDataSource().getResident(playerName);
+
+                        importedWar.addPlayerToWar(resident.getTown(), resident.getUUID());
+                        Message.IMPORTER_INFO.send(sender, ChatColor.GREEN + "Successfully added " + ChatColor.GREEN + playerName + ChatColor.GREEN + " to participants");
+                    }
+                } catch (NotRegisteredException e) {
+                    Message.IMPORTER_INFO.send(sender, ChatColor.RED + "Nation or player not registered on Towny, please fix the name and run the importer again");
+                    return;
+                } catch (EconomyException e) {
+                    warService.deleteWar();
+                }
+
+                if(postList.indexOf(post) == postList.size() - 1) {
+                    Message.IMPORTER_INFO.send(sender, ChatColor.GREEN + "War successfully imported, you may now proceed with default commands for the war type");
+                    warService.setCurrentWar(importedWar);
+                }
+            }
+        }, (error) -> {
+            sender.sendMessage(error);
+        });
+    }
+
+}
